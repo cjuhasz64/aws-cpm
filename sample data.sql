66e524df7e3b4259f36000b10210ab4778a901b8
103526884 <105096809+103526884@users.noreply.github.com>
2022-05-07 16:16:09 +1000

    Add files via upload

---
+++ sample data.sql
@@ -0,0 +1,70 @@
+DROP TABLE IF EXISTS `myTable`;
+
+CREATE TABLE `myTable` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `email` varchar(255) default NULL,
+  `phone` varchar(100) default NULL,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
+
+INSERT INTO `myTable` (`name`,`email`,`phone`)
+VALUES
+  ("Basia Hammond","diam.dictum.sapien@protonmail.org","(07) 0500 4127"),
+  ("Brenna Sandoval","pellentesque.massa@outlook.ca","(07) 5052 0325"),
+  ("Steven Robbins","mi.duis.risus@google.couk","(04) 0137 3300"),
+  ("Dante Ayala","natoque.penatibus@yahoo.ca","(01) 9171 5171"),
+  ("Lacy Joyce","dapibus.id@icloud.couk","(06) 3225 4369"),
+  ("Beau Arnold","dui.lectus@outlook.net","(03) 4724 4488"),
+  ("Salvador Cantu","sem.vitae@hotmail.com","(08) 0715 3336"),
+  ("Benedict Dodson","natoque@protonmail.com","(09) 9874 5438"),
+  ("Solomon Roy","lobortis.class@icloud.edu","(07) 2105 7077"),
+  ("Neville Alston","at@yahoo.org","(06) 4425 7924");
+INSERT INTO `myTable` (`name`,`email`,`phone`)
+VALUES
+  ("Delilah Douglas","arcu@google.couk","(08) 2672 8824"),
+  ("Hanna Perry","nam.porttitor@outlook.net","(07) 5156 8802"),
+  ("Jesse Everett","nunc.mauris@hotmail.ca","(09) 4155 0327"),
+  ("Brandon Frost","arcu.vestibulum.ante@yahoo.couk","(05) 6414 6163"),
+  ("Britanni Barlow","magna@protonmail.couk","(02) 7022 7664"),
+  ("Armando Gould","posuere.vulputate@outlook.org","(07) 5259 3685"),
+  ("Stewart Lang","duis.ac@yahoo.com","(08) 5345 3214"),
+  ("Kay Spears","urna@yahoo.ca","(09) 6413 3631"),
+  ("Damian Johnson","bibendum@icloud.couk","(03) 3666 5815"),
+  ("Griffith Cooper","luctus.lobortis@outlook.ca","(04) 4449 5236");
+INSERT INTO `myTable` (`name`,`email`,`phone`)
+VALUES
+  ("Finn Gillespie","semper.dui@google.ca","(01) 3594 7447"),
+  ("Rosalyn Blankenship","donec@aol.net","(03) 3465 5538"),
+  ("Nyssa Rojas","vulputate@outlook.couk","(07) 9421 4380"),
+  ("Chastity Frye","in.dolor@outlook.com","(04) 4603 4655"),
+  ("Lilah Pace","risus@icloud.couk","(06) 8382 1036"),
+  ("Regan Johnston","eget@aol.couk","(03) 2697 2217"),
+  ("Cadman Buck","porttitor.vulputate@aol.couk","(01) 7302 4325"),
+  ("Cleo Jennings","aenean.sed@protonmail.org","(06) 3342 2091"),
+  ("Nora Benson","euismod.mauris.eu@outlook.com","(09) 8637 2932"),
+  ("Raja Kramer","tempor.diam@icloud.ca","(08) 3162 4415");
+INSERT INTO `myTable` (`name`,`email`,`phone`)
+VALUES
+  ("Lavinia Patterson","arcu.curabitur@yahoo.com","(06) 8277 1348"),
+  ("Miranda Riddle","ante.iaculis.nec@icloud.couk","(06) 3207 6750"),
+  ("Urielle Smith","aliquam.vulputate.ullamcorper@aol.edu","(05) 3824 7334"),
+  ("Ulla Blevins","eget.venenatis.a@aol.com","(04) 3377 6195"),
+  ("Cleo Ortiz","congue.elit@yahoo.ca","(06) 1471 2240"),
+  ("Nevada Mclaughlin","aliquet.diam.sed@protonmail.com","(02) 4761 3567"),
+  ("April Green","dictum.sapien@hotmail.com","(05) 8672 1746"),
+  ("Callum Hooper","donec@google.net","(02) 7218 5956"),
+  ("Haviva Wolf","dictum.sapien@google.net","(05) 8955 9821"),
+  ("Yuli Leonard","vitae.aliquam.eros@protonmail.org","(02) 4590 4770");
+INSERT INTO `myTable` (`name`,`email`,`phone`)
+VALUES
+  ("Hilel Bond","lorem.ipsum.dolor@aol.couk","(01) 1581 8372"),
+  ("Ross Graves","eu.metus.in@hotmail.org","(09) 5131 2366"),
+  ("Zenaida Colon","vulputate.eu@yahoo.com","(08) 1057 1556"),
+  ("Octavius Cooper","arcu.imperdiet.ullamcorper@outlook.ca","(04) 1477 9945"),
+  ("Riley Patterson","nec.ligula@hotmail.com","(09) 1719 7902"),
+  ("Odette Cameron","aliquam.fringilla@yahoo.net","(02) 6492 4370"),
+  ("Giselle Camacho","elit@yahoo.com","(02) 5813 5016"),
+  ("Demetria Cameron","lobortis.quam@hotmail.com","(05) 5659 8652"),
+  ("Pascale Townsend","imperdiet.nec@google.ca","(02) 7561 2197"),
+  ("Stuart Anthony","lacus.ut@aol.couk","(08) 6433 6208");
