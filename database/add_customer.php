61e95e7115469b74ea4807a34c0a30205bb0b532
103526884 <105096809+103526884@users.noreply.github.com>
2022-05-08 19:22:31 +1000

    Create add_customer.php

---
+++ database/add_customer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>GFG- Store Data</title>
+</head>
+
+<body>
+    <center>
+        <h1>Testing page for adding customer to database</h1>
+  
+        <form action="insert.php" method="post">
+          
+          <p>
+            <label for="Name">Full Name:</label>
+            <input type="text" name="name" id="Name">
+          </p>
+          
+          <p>
+            <label for="Phone">Phone:</label>
+            <input type="text" name="phone" id="phone">
+          </p>
+                   
+          <p>
+            <label for="email">Email Address:</label>
+            <input type="text" name="email" id="email">
+          </p>
+          
+         <input type="submit" value="Submit">
+      
+       </form>
+    </center>
+</body>
+  
+</html>
