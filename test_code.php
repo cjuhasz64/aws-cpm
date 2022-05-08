7902babc32bbea28e3654a98fc2a25632ee0a1ae
103526884 <105096809+103526884@users.noreply.github.com>
2022-05-09 07:52:42 +1000

    Update test_code.php

--- test_code.php
+++ test_code.php
@@ -1,27 +1,15 @@
 <?php
+// servername => localhost
+// username => root
+// password => empty
+// database name => users
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "database_name";
-
-// Create connection
-$conn = new mysqli($servername,
-	$username, $password, $dbname);
-
+$conn = mysqli_connect("localhost", "root", "", "aws_cpm");
+          
 // Check connection
-if ($conn->connect_error) {
-	die("Connection failed: "
-		. $conn->connect_error);
-}
-
-$sqlquery = "INSERT INTO table VALUES
-	('John', 'Doe', 'john@example.com')"
-
-if ($conn->query($sql) === TRUE) {
-	echo "record inserted successfully";
-} else {
-	echo "Error: " . $sql . "<br>" . $conn->error;
+if($conn === false){
+	die("ERROR: Could not connect. " 
+	    . mysqli_connect_error());
 }
 
 ?>
