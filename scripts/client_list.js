b15f1843c8ea54458805c105ed2c680d1e00844f
cjuhasz64 <cjuhasz64@gmail.com>
2022-05-17 14:15:57 +1000

    client list implement

--- scripts/client_list.js
+++ scripts/client_list.js
@@ -1,7 +1,54 @@
 
 
-var tempClients = ["cameron", "jack", "lachlan", "kyle", "zach"];
+var expClients = ["Cameron", "Jack", "Lachlan", "Kyle", "Zach", "Greg", "David", "Theo", "Jill", "June", "Jose"];
 
-tempClients.forEach(client => {
-    document.getElementById('client-list').innerHTML += `<a href=""><div class="item">${client}</div></a>`
+expClients.sort();
+
+expClients.forEach(client => {
+    document.getElementById('client-list').innerHTML += `<div class="item" onclick="displayDetailed('${client}')">${client}</div>`
 });
+
+function sortClients() {
+
+    let keyword = document.getElementById("client-search").value.toLowerCase()
+
+    let tempClients = [];
+
+    expClients.forEach(client => {
+        tempClients.push(client.toLowerCase())
+    });
+
+
+    let sortedClients = [];
+    tempClients.forEach(client => {
+        if (client.startsWith(keyword))
+        {
+            sortedClients.push(client);
+        }
+
+    });
+    
+    document.getElementById('client-list').innerHTML = "";
+    
+
+    if (sortedClients.length == 0)
+    {
+        document.getElementById('client-list').innerHTML = "no results";
+    }
+    else
+    {
+        sortedClients.forEach(client => {
+            document.getElementById('client-list').innerHTML += `<div class="item" onclick="displayDetailed('${client}')">${client}</div>`
+        });
+    
+    } 
+}
+
+function displayDetailed(clientName)
+{
+    // just exmple of how details will be displayed
+    // actual applicaiton would be haivng a client object that
+    // gets data from a database to display in this secton 
+    document.getElementById('example-client').innerHTML = clientName;
+  
+}
